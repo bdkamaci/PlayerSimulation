28fdc6dac744d74168344068679dc5b073cfef48
bdkamaci <burcudogakamaci@gmail.com>
2025-04-17 10:46:26 +0200

    refactor: update MessageQueue to use Message record

--- src/main/java/tech/MessageQueue.java
+++ src/main/java/tech/MessageQueue.java
@@ -1,30 +1,65 @@
 package tech;
 
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Manages message queues for each player in the game.
+ * Each player has a dedicated queue identified by their name.
+ */
 public class MessageQueue {
-    private final BlockingQueue<String> queue;
+
     private final Logger logger = LoggerFactory.getLogger(MessageQueue.class);
 
-    public MessageQueue() {
-        this.queue = new LinkedBlockingQueue<>();
+    private final Map<String, BlockingQueue<Message>> queues = new ConcurrentHashMap<>();
+
+    /**
+     * Registers a player with a dedicated message queue.
+     *
+     * @param playerName the name of the player
+     */
+    public void registerPlayer(String playerName) {
+        queues.putIfAbsent(playerName, new LinkedBlockingQueue<>());
+        logger.info("Registered message queue for {}", playerName);
     }
 
-    public void put(String message) throws InterruptedException {
-        queue.put(message);
-        logger.info("Message put into queue: {}", message);
+    /**
+     * Sends a message to the specified player.
+     *
+     * @param message the message to send
+     * @throws InterruptedException if interrupted while adding to the queue
+     */
+    public void sendMessage(Message message) throws InterruptedException {
+        BlockingQueue<Message> queue = queues.get(message.receiver());
+        if (queue != null) {
+            queue.put(message);
+            logger.info("Message sent: {}", message);
+        } else {
+            logger.warn("Queue not found for receiver: {}", message.receiver());
+        }
     }
 
-    public String take() throws InterruptedException {
-        String message = queue.take();
-        logger.info("Message taken from queue: {}", message);
-        return message;
-    }
-
-    public BlockingQueue<String> getQueue() {
-        return queue;
+    /**
+     * Retrieves the next message for a player.
+     *
+     * @param playerName the name of the player
+     * @return the next message
+     * @throws InterruptedException if interrupted while waiting
+     */
+    public Message receiveMessage(String playerName) throws InterruptedException {
+        BlockingQueue<Message> queue = queues.get(playerName);
+        if (queue != null) {
+            Message msg = queue.take();
+            logger.info("{} received: {}", playerName, msg);
+            return msg;
+        } else {
+            logger.warn("Queue not found for {}", playerName);
+            return null;
+        }
     }
 }
